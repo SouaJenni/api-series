747bab1f0769d67fad182e70cb3587d941e05d5f
SouaJenni <jennifervialli1@gmail.com>
2025-07-12 21:13:44 -0300

    Adicionando regras para não permitir importes e variáveis não utilizadas no ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,43 @@
 const js = require('@eslint/js');
 const globals = require('globals');
+const unusedImports = require('eslint-plugin-unused-imports');
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 module.exports = [
     {
-        files: ['**/*.{js,cjs}'],
+        files: ['**/*.{js,jsx,cjs}'],
         ...js.configs.recommended,
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        languageOptions: {
+            sourceType: 'module',
+            ecmaVersion: 2022,
+            globals: {
+                ...globals.browser,
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
         rules: {
             indent: ['error', 4],
             quotes: ['error', 'single'],
             semi: ['error', 'always'],
-        },
-        languageOptions: {
-            sourceType: 'commonjs',
-            globals: {
-                ...globals.browser,
-            },
+
+            'no-unused-vars': 'off',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    varsIgnorePattern: '^_',
+                    args: 'after-used',
+                    argsIgnorePattern: '^_',
+                },
+            ],
         },
     },
 ];
